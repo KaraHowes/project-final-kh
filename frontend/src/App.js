d2e664bb572b57e20a0cd4718a7bc237884d928a
Kara Howes <karahowes@Karas-MacBook-Air.local>
2022-02-04 22:43:14 +0100

    feat: inspirations page fully implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,61 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { NotFound } from "./pages/NotFound";
+import Welcome from "./pages/Welcome";
+import Profile from "./pages/Profile";
+import AddThek from "./pages/AddThek";
+import AllBags from "./pages/AllBags";
+import FindThek from "./pages/FindThek";
+import BagAdded from "./pages/BagAdded";
+import SelectedBag from "./pages/SelectedBag";
+import Starter from "./pages/Starter";
+import Intro from "./pages/Intro";
+import BagsFound from "./pages/BagsFound";
+import Inspirations from "./pages/Inspirations";
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { NotFound } from './pages/NotFound';
-import Welcome from './pages/Welcome'
-import Profile from './pages/Profile'
-import AddThek from './pages/AddThek'
-import AllBags from './pages/AllBags'
-import FindThek from './pages/FindThek'
-import BagAdded from './pages/BagAdded'
-import SelectedBag from './pages/SelectedBag'
-import Starter from './pages/Starter'
-import Intro from './pages/Intro'
-import BagsFound from './pages/BagsFound'
-import Inspirations from './pages/Inspirations'
-
-import member from './reducers/member';
-import theks from './reducers/theks';
-import oneThek from './reducers/oneThek'
-import searched from './reducers/searched'
+import member from "./reducers/member";
+import theks from "./reducers/theks";
+import oneThek from "./reducers/oneThek";
+import searched from "./reducers/searched";
+import quote from "./reducers/quote";
 
 const reducer = combineReducers({
-	member: member.reducer,
-	theks: theks.reducer,
-	oneThek: oneThek.reducer,
-	searched: searched.reducer,
+  member: member.reducer,
+  theks: theks.reducer,
+  oneThek: oneThek.reducer,
+  searched: searched.reducer,
+  quote: quote.reducer,
 });
 
 const store = configureStore({ reducer });
 
 export const App = () => {
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/starter" element={<Starter />}/>
-					<Route path="/member/:memberId" element={<Profile />} />
-					<Route path="/welcome" element={<Welcome />} />
-					<Route path="/signin" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/AddThek" element={<AddThek />} />
-					<Route path="/FindThek" element={<FindThek />} />
-					<Route path="/AllBags" element={<AllBags />} />
-					<Route path="/BagAdded" element={<BagAdded />} />
-					<Route path="/bag/:_id" element={<SelectedBag />} />
-					<Route path="/intro" element={<Intro />} />
-					<Route path="/inspiration" element={<Inspirations />} />
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/starter" element={<Starter />} />
+          <Route path="/member/:memberId" element={<Profile />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/AddThek" element={<AddThek />} />
+          <Route path="/FindThek" element={<FindThek />} />
+          <Route path="/AllBags" element={<AllBags />} />
+          <Route path="/BagAdded" element={<BagAdded />} />
+          <Route path="/bag/:_id" element={<SelectedBag />} />
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/inspiration" element={<Inspirations />} />
 
-					<Route path="/bagsFound" element={<BagsFound />} />
-					<Route path="*" element={<NotFound />} />
-
-				</Routes>
-			</BrowserRouter>
-		</Provider>
-	);
+          <Route path="/bagsFound" element={<BagsFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
--- frontend/src/pages/Inspirations.js
+++ frontend/src/pages/Inspirations.js
@@ -1,26 +1,51 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch, batch } from "react-redux";
 
+import quote from '../reducers/quote'
 import { API_URL } from "../utils/urls";
 
 
 const Div = styled.div`
-background-color: white;`
+background-color: white;
+border: solid 5px black;`
+
+
 const Inspirations = () => {
-  const [affirmation, setAffirmation] = useState("");
+
+  const affirmation = useSelector((store)=> store.quote)
+  const dispatch = useDispatch();
+
+
 
   useEffect(() => {
     fetch(API_URL("inspiration"))
       .then((res) => res.json())
-      .then((response) => {
-        setAffirmation(response);
-        console.log(response)
+      .then((data) => {
+        
+        console.log(data)
+        if (data.success) {
+          batch(() => {
+            dispatch(quote.actions.set_Id(data.response._id));
+            dispatch(quote.actions.setQuote(data.response.quote));
+            dispatch(quote.actions.setSource(data.response.source));
+            dispatch(quote.actions.setError(null));
+          });
+        } else {
+          batch(() => {
+            dispatch(quote.actions.set_Id(null));
+            dispatch(quote.actions.setQuote(null));
+            dispatch(quote.actions.setSource(null));
+            dispatch(quote.actions.setError(data.response));
+          });
+        }
       });
   }, []);
 
   return (
     <Div>
-      {affirmation.quote}
+      <h1>{affirmation.quote}</h1>
+      <h1>{affirmation.source}</h1>
     </Div>
   );
 };
